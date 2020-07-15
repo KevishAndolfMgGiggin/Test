d38d0c66f805d0998c7f509f059832e4a9e83679
Kevish <kevish.andolf.mggiggin@gmail.com>
2020-07-15 16:30:28 -0400

    Character Movement
    Added basic WASD controls with a following 3rd person camera. Movement is forward and backward with rotation left and right.

---
+++ GitHubDesktopTest/Assets/IcoMan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcoMan : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    public float forwardSpeed;
+    public float backSpeed;
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            _controller.Move(transform.forward * Time.deltaTime * forwardSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _controller.Move(transform.forward * -1 * Time.deltaTime * backSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _controller.transform.Rotate(Vector3.down * turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _controller.transform.Rotate(Vector3.up * turnSpeed);
+        }
+    }
+}
